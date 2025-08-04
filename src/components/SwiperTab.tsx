dbd51f56168232f1d75e12bc47a774d59d3833c8
1il1day <jgffdet@nate.com>
2025-08-05 00:01:34 +0900

    SwiperTab 컴포넌트 추가

---
+++ src/components/SwiperTab.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import styled from 'styled-components';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/effect-fade';
+import 'swiper/css/thumbs';
+
+type TabData = {
+  title: string;
+  contents?: React.ReactNode;
+}
+type Props = {
+  data: TabData[];
+  useContents?: boolean;
+}
+
+/**
+ * @param [props.data] - 탭 제목, 탭 내용 데이터
+ * @param [props.useContents] - true일 경우, 탭 내용 표시
+ */
+export default function SwiperTab({data, useContents = false}: Props) {
+  const [activeIndex, setActiveIndex] = useState(3);
+
+  return (
+    <>
+      <div>
+        {/* 탭 제목 (swiper) */}
+        <TabWrap
+          freeMode={true}
+          slidesPerView={3}
+          watchSlidesProgress={true}
+          onClick={(swiper:any) => {
+            if(typeof swiper.clickedIndex == 'number'){
+              setActiveIndex(swiper.clickedIndex)}}
+            }
+          breakpoints={
+            {
+              0: {
+                slidesPerView: 3.5,
+              },
+              350: {
+                slidesPerView: 4,
+                spaceBetween: 24,
+              },
+            }
+          }
+        >
+          {data.map((item,index)=>(
+            <TabList key={index} className={index === activeIndex ? "active" : ""}>{item.title}</TabList>
+          ))}
+        </TabWrap>
+      </div>
+      {/* 탭 내용 */}
+      <div className="terminal-text-wrap">
+        <span>{">"}</span>
+        {useContents && data[activeIndex].contents}
+      </div>
+    </>
+  )
+}
+
+const TabWrap = styled(Swiper)`
+  position: relative;
+  padding: 13px 20px 20px;
+  font-size: 14px;
+  color: #9B9DA0;
+  @media (min-width: 768px) {
+    padding: 13px 0 35px;
+    font-size: 16px;
+  }
+`;
+const TabList = styled(SwiperSlide)`
+  text-align: center;
+  cursor: pointer;
+  @media (min-width: 350px) {
+    width: auto !important;
+    padding: 0px;
+  }
+  &.active{
+    position: relative;
+    color: #CFD0D1;
+    &::after{
+      content: "";
+      display: block;
+      position: absolute;
+      bottom: -5px;
+      left: 0;
+      width: 100%;
+      height: 1px;
+      background-color: #CFD0D1;
+    }
+  }
+`;
